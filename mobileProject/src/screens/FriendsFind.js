402616eb36883dc76f8831bd731c94d28d0ff108
parkjunyeap <bab85503@gmail.com>
2023-11-06 22:04:57 +0900

    친구찾기 버튼만
    친구찾기버튼만일단

--- mobileProject/src/screens/FriendsFind.js
+++ mobileProject/src/screens/FriendsFind.js
@@ -1,18 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const FriendsFInd = () => {
+  const navigation = useNavigation();
+
+  const navigateToSearchPage = () => {
+    // Navigate to the SearchPage component
+    navigation.navigate("SearchPage");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>친구찾기</Text>
-    </View>
+    // 친구 찾기 누르면 친구 찾기 페이지로 이동
+
+    <TouchableOpacity style={styles.button} onPress={navigateToSearchPage}>
+      <Text style={styles.buttonText}>친구 찾기</Text>
+    </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  button: {
+    // Match the style with your design
+    backgroundColor: "#4CAF50", // This is the green background
+    padding: 15,
+    borderRadius: 5,
     justifyContent: "center",
     alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    // Add other styles for your text if needed
   },
 });
 
