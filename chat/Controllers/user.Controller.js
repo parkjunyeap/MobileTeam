79e8dd379dc244eaa0b7dd699c3b8169a4dcf80c
dayzononedaypack <whathumen@gmail.com>
2023-11-15 18:27:13 +0900

    ㅇ

---
+++ chat/Controllers/user.Controller.js
@@ -0,0 +1,19 @@
+const userController ={}
+const User = require("../Models/user")
+
+userController.saveUser=async(userName,sid)=>{
+    let user = await User.findOne({name:userName})  
+    if (!user){
+        user = new User({
+            name:userName,
+            token:sid,
+            online:true,
+        })
+    }
+    user.token=sid
+    user.online=true
+
+    await user.save()
+    return user
+}
+module.exports = userController
--- chat/utils/io.js
+++ chat/utils/io.js
@@ -1,8 +1,20 @@
-module.exports = function(io) {
+const userController = require("../Controllers/user.Controller")
+
+module.exports = function (io) {
     io.on("connection", async (socket) => {
         console.log("Client is connected", socket.id)
 
-        socket.on("disconnect", ()=>{
+        socket.on("login", async (userName, cb) => {
+            //유저 정보 저장
+            try {
+                const user = await userController.saveUser(userName, socket.id)
+                cb({ ok: true, data: user })
+            } catch (error) {
+                cb({ ok: false, error: error.message })
+            }
+        })
+
+        socket.on("disconnect", () => {
             console.log("user disconnected", socket.id)
         })
     })
