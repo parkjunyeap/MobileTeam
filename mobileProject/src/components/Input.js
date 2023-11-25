14ace9736a1ab41fa9c6977153ef15fd85ef8618
parkjunyeap <bab85503@gmail.com>
2023-11-25 12:20:39 +0900

    로그인 화면 input 수정 깔끔하게

--- mobileProject/src/components/Input.js
+++ mobileProject/src/components/Input.js
@@ -1,47 +1,44 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import PropTypes from 'prop-types';
-import { forwardRef, useState } from 'react';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { GRAY, PRIMARY } from '../colors';
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import PropTypes from "prop-types";
+import { forwardRef, useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { GRAY, PRIMARY } from "../colors";
 
-export const KeyboardTypes = {
-  DEFAULT: 'default',
-  EMAIL: 'email-address',
-};
-
-export const ReturnKeyTypes = {
-  DONE: 'done',
-  NEXT: 'next',
-};
 export const InputTypes = {
-  EMAIL: 'EMAIL',
-  PASSWORD: 'PASSWORD',
-  PASSWORD_CONFIRM: 'PASSWORD_CONFIRM',
+  EMAIL: "EMAIL",
+  PASSWORD: "PASSWORD",
+  PASSWORD_CONFIRM: "PASSWORD_CONFIRM",
 };
 
 const PasswordProps = {
-  keyboardType: 'default',
+  keyboardType: "default",
   secureTextEntry: true,
-  iconName: { active: 'lock', inactive: 'lock-outline' },
+  iconName: { active: "lock", inactive: "lock-outline" },
 };
+
 const InputTypeProps = {
   EMAIL: {
-    title: 'EMAIL',
-    placeholder: 'your@email.com',
-    KeyboardTypes: 'email-address',
+    title: "EMAIL",
+    placeholder: "your@email.com",
+    keyboardType: "email-address",
     secureTextEntry: false,
-    iconName: { active: 'email', inactive: 'email-outline' },
+    iconName: { active: "email", inactive: "email-outline" },
   },
   PASSWORD: {
-    title: 'PASSWORD',
-    placeholder: 'PASSWORD',
-    ...PasswordProps, // 중복되는 값 따로 꺼냄
-  },
-  PASSWORD_CONFIRM: {
-    title: 'PASSWORD CONFIRM',
-    placeholder: 'PASSWORD CONFIRM',
+    title: "PASSWORD",
+    placeholder: "PASSWORD",
     ...PasswordProps,
   },
+  PASSWORD_CONFIRM: {
+    title: "PASSWORD_CONFIRM",
+    placeholder: "PASSWORD_CONFIRM",
+    ...PasswordProps,
+  },
+};
+
+export const ReturnKeyTypes = {
+  DONE: "done",
+  NEXT: "next",
 };
 
 const Input = forwardRef(({ inputType, styles, ...props }, ref) => {
@@ -53,9 +50,8 @@ const Input = forwardRef(({ inputType, styles, ...props }, ref) => {
     iconName: { active, inactive },
   } = InputTypeProps[inputType];
 
+  const [isFocused, setIsFocused] = useState(false);
   const { value } = props;
-
-  const [isFocused, setIsFocused] = useState(false); // 포커스 상태에 따라 다른 아이콘을 사용하도록 포커스 상태 관리하는 상태변수래
 
   return (
     <View style={[defaultStyles.container, styles?.container]}>
@@ -68,15 +64,11 @@ const Input = forwardRef(({ inputType, styles, ...props }, ref) => {
       >
         {title}
       </Text>
+
       <View>
         <TextInput
           ref={ref}
           {...props}
-          placeholder={placeholder}
-          keyBoardType={keyboardType}
-          secureTextEntry={secureTextEntry}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
           style={[
             defaultStyles.input,
             {
@@ -85,13 +77,19 @@ const Input = forwardRef(({ inputType, styles, ...props }, ref) => {
             },
             styles?.input,
           ]}
-          textContentType="none"
-          autoCapitalize="none"
+          placeholder={placeholder}
+          keyboardType={keyboardType}
+          secureTextEntry={secureTextEntry}
+          onBlur={() => setIsFocused(false)}
+          onFocus={() => setIsFocused(true)}
+          autoCapitalize={"none"}
           autoCorrect={false}
+          textContentType={"none"}
+          keyboardAppearance={"light"}
         />
-        <View style={(defaultStyles.icon, styles?.icon)}>
-          <MaterialCommunityIcons // 아이콘 모음 리액트네이티브
-            name={isFocused ? active : inactive} // 지금 선택했으면 active
+        <View style={[defaultStyles.icon, styles?.icon]}>
+          <MaterialCommunityIcons
+            name={isFocused ? active : inactive}
             size={24}
             color={value || isFocused ? PRIMARY.DEFAULT : GRAY.DARK}
           />
@@ -100,37 +98,33 @@ const Input = forwardRef(({ inputType, styles, ...props }, ref) => {
     </View>
   );
 });
-
-Input.displayName = 'Input';
+Input.displayName = "Input";
 
 Input.propTypes = {
-  // title: PropTypes.string,
-  // iconName: PropTypes.string,
-  inputType: PropTypes.oneOf(Object.values(InputTypes)).isRequired,
-  value: PropTypes.string.isRequired,
+  inputType: PropTypes.oneOf(Object.values(InputTypes)),
+  value: PropTypes.string,
   styles: PropTypes.object,
 };
 
 const defaultStyles = StyleSheet.create({
   container: {
-    width: '100%',
+    width: "100%",
   },
   title: {
     marginBottom: 4,
-    fontWeight: '700',
+    fontWeight: "700",
   },
   input: {
     borderBottomWidth: 1,
-    borderRadius: 8,
     height: 42,
     paddingHorizontal: 10,
     paddingLeft: 40,
   },
   icon: {
-    position: 'absolute',
+    position: "absolute",
     left: 8,
-    height: '100%',
-    justifyContent: 'center',
+    height: "100%",
+    justifyContent: "center",
   },
 });
 
