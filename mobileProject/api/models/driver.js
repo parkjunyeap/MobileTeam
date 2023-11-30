9837d259690cff547bd9a8a9b823e29fe2fda99e
dayzononedaypack <whathumen@gmail.com>
2023-11-30 20:25:20 +0900

    model 수정

--- mobileProject/api/models/driver.js
+++ mobileProject/api/models/driver.js
@@ -18,16 +18,15 @@ const driverSchema = new mongoose.Schema({
     type: String,
     required: true, // 사용자 프로필 이미지, 필수 입력 필드
   },
-
-  personalNumber: {
-    type: String,
-    required: true,
-  },
   carNumber: {
     type: String,
     required: true,
   },
-  driverLicense: {
+  carName: {
+    type: String,
+    required: true,
+  },
+  licenseNumber: {
     type: String,
     required: true,
   },
@@ -38,34 +37,8 @@ const driverSchema = new mongoose.Schema({
     required: true,
     default: false,
   },
-
-  taxiReview: [
-    {
-      writerId: {
-        type: mongoose.Schema.Types.ObjectId, // 택시 기사를 참조하는 ID
-        ref: "User",
-      }, // 리뷰를 보여줄때는 .name 만 불러오면됨.
-      driverId: {
-        type: mongoose.Schema.Types.ObjectId, // 택시 기사를 참조하는 ID
-        ref: "Driver",
-      }, //
-      rating: {
-        type: Number, // 평점 (예: 1에서 5까지)
-        required: true,
-      },
-      comment: {
-        type: String, // 리뷰 내용
-        required: false,
-      },
-      reviewDate: {
-        type: Date, // 리뷰 작성 날짜
-        default: Date.now,
-      },
-      // 필요한 경우 추가적인 필드를 포함할 수 있습니다.
-    },
-  ],
 });
 
-const Driver = mongoose.model("driver", driverSchema); // 'User' 모델로 스키마 컴파일
+const Driver = mongoose.model("Driver", driverSchema); // 'User' 모델로 스키마 컴파일
 
 module.exports = Driver; // 모듈로 내보내기, 다른 파일에서 사용 가능
