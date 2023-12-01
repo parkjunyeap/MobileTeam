954279f5de68c1b32dd66b3900b2c22591b72583
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-12-01 18:47:11 +0900

    지도나옴(앱전용)
    더러운지도

--- mobileProject/src/screens/TaxiTouch.js
+++ mobileProject/src/screens/TaxiTouch.js
@@ -1,14 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const TaxiTouch = () => {
   return (
-    <View style={{ alignItems: "center", justifyContent: "center" }}>
+    <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
+        initialRegion={{
+          latitude: 37.78825, // 원하는 좌표로 변경하세요
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
       >
-        {/* 여기서 지도 관련 컨텐츠를 추가할 수 있습니다. */}
+        {/* 추가적인 지도 요소를 배치하세요 */}
       </MapView>
     </View>
   );
@@ -17,12 +26,12 @@ const TaxiTouch = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   map: {
-    width: 300, // 원하는 너비 설정
-    height: 300, // 원하는 높이 설정
+    width: SCREEN_WIDTH,
+    height: '100%',
   },
 });
 
