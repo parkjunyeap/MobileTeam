490159d4b59bc48ce3afa4c71e322fb13880c25a
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-12-05 20:27:47 +0900

    마커는 나옴
    왜 안움직이냐

--- mobileProject/src/screens/TaxiTouch.js
+++ mobileProject/src/screens/TaxiTouch.js
@@ -1,63 +1,43 @@
-import React, { useState, useRef } from 'react';
-import { View, Image, Text, StyleSheet , SafeAreaView} from 'react-native';
-import MapView, { Callout, PROVIDER_GOOGLE, Marker, Circle, Polyline, Polygon } from 'react-native-maps';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'; //으악 이거 입력 ㅠㅠ
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Image, Text, StyleSheet, SafeAreaView } from 'react-native';
+import MapView, { Callout, PROVIDER_GOOGLE,Marker } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_API_KEY } from '../config/constants';
 import MapViewDirections from 'react-native-maps-directions';
 
 
 export default function GoogleMapsScreen() {
   const mapRef = useRef(null);
-  const [origin, setOrigin] = useState();
-  const [destination, setDestination] = useState();
-  const [markersList, setMarkerList] = useState([
-    
-    // 마커 찍고 싶은 위치 쭈루루룩 넣어줌 (연습용)
-    {
-      id: 1,
-      latitude: 36.7898,
-      longitude: 127.0012,
-      title: 'team1',
-      description: 'ddd'
-    },
-    {
-      id: 2,
-      latitude: 37.28825,
-      longitude: 127.1012,
-      title: 'team2',
-      description: 'ddd'
-    },
-  ]);
+  // 초기 상태를 null에서 명시적으로 설정합니다.
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+  const [routeCoordinates, setRouteCoordinates] = useState([]);
+  const [movingMarkerPosition, setMovingMarkerPosition] = useState(null);
 
+  useEffect(() => {
+    if (origin) {
+      setMovingMarkerPosition(origin);
+    }
+  }, [origin]);
 
-//  커스텀마커뷰 _ 자동차 이미지!!!!
   const MyCustomMarkerView = () => {
     return (
       <Image
-        style={{
-          width: 30,
-          height: 30,
-        }}
-        source={require('../assets/car.png')}
+        style={{ width: 30, height: 30 }}
+        source={require('../../assets/carMarker.png')}
       />
     );
   };
 
-// 컬아웃 텍스트
   const MyCustomCalloutView = () => {
     return (
       <View>
-        <Text>
-          MyCustomCalloutVieww22
-        </Text>
+        <Text>MyCustomCalloutVieww22</Text>
       </View>
     );
   };
 
-
-  //  설정한 화면으로 자동으로 이동함
   async function moveToLocation(latitude, longitude) {
-    console.log(`Moving to latitude: ${latitude}, longitude: ${longitude}`);
     mapRef.current.animateToRegion(
       {
         latitude,
@@ -69,14 +49,23 @@ export default function GoogleMapsScreen() {
     );
   }
 
+  const animateMarker = () => {
+    let step = 0;
+    const numSteps = routeCoordinates.length - 1;
+    const interval = setInterval(() => {
+      // 마커 위치를 업데이트합니다.
+      if (step <= numSteps) {
+        setMovingMarkerPosition(routeCoordinates[step]);
+        step++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 1000);
+  };
 
-
-  // 화면 구성
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.searchContainer}> 
-      
-      {/* 출발지 검색 */}
+      <View style={styles.searchContainer}>
         <GooglePlacesAutocomplete
           fetchDetails={true}
           placeholder='출발지'
@@ -91,17 +80,14 @@ export default function GoogleMapsScreen() {
             key: GOOGLE_MAPS_API_KEY,
             language: 'ko',
           }}
-          onFail={(error) => console.log(error)}
+          onFail={(error) => console.error(error)}
         />
-
-        {/* 목적지 검색 */}
         <GooglePlacesAutocomplete
           fetchDetails={true}
           placeholder='목적지'
           onPress={(data, details = null) => {
             if (details) {
               const { lat, lng } = details.geometry.location;
-              console.log(`Destination coordinates: Latitude: ${lat}, Longitude: ${lng}`);
               setDestination({ latitude: lat, longitude: lng });
               moveToLocation(lat, lng);
             }
@@ -110,113 +96,61 @@ export default function GoogleMapsScreen() {
             key: GOOGLE_MAPS_API_KEY,
             language: 'ko',
           }}
-          onFail={(error) => console.log(error)}
-      />
-    </View>
-
-    {/* 지도 구성 */}
-    <MapView
-      ref={mapRef}
-      provider={PROVIDER_GOOGLE}
-      style={styles.map}
-      region={{
-        latitude: 36.47,
-        longitude: 127.43,
-        latitudeDelta: 0.1,
-        longitudeDelta: 0.1,
-      }}>
-
-
-        {origin !== undefined && (
-          <Marker 
-            draggable 
-            coordinate={origin} 
-            onDragEnd={(e) => console.log('출발지 드래그 한 좌표 :',{  x: e.nativeEvent.coordinate })}/>
-        )}
-        {destination !== undefined && (
-          <Marker 
-            draggable 
-            coordinate={destination} 
-            onDragEnd={(e) => console.log('목적지 드래그 한 좌표: ',{  x: e.nativeEvent.coordinate })}/>
-        )}
-
-          {/* 마커 연습 _ 커스텀마커뷰(자동차 이미지) 콜아웃(텍스트) */}
-        <Marker
-          coordinate={{ latitude: 36.800131, longitude: 127.074941 }}>
-          <MyCustomMarkerView />
-          <Callout style={{ width: 300, height: 100, backgroundColor: 'white' }}>
-            <MyCustomCalloutView />
-          </Callout>
-        </Marker>
- 
-          {/* 드래그 */}
-        {markersList.map((marker) => (
+          onFail={(error) => console.error(error)}
+        />
+      </View>
+      <MapView
+        ref={mapRef}
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 36.47,
+          longitude: 127.43,
+          latitudeDelta: 0.1,
+          longitudeDelta: 0.1,
+        }}>
+        {origin && (
           <Marker
             draggable
-            key={marker.id}
-            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
-            title={marker.title}
-            description={marker.description}
-            onDragEnd={(e) => console.log({ x: e.nativeEvent.coordinate })}
+            coordinate={origin}
+            onDragEnd={(e) => console.log('출발지 드래그 좌표: ', e.nativeEvent.coordinate)}
           />
-        ))}
-
-          {/* 원형 */}
-        <Circle
-          center={{ latitude: 36.7898, longitude: 127.0012 }}
-          radius={200}
-          strokeColor="blue"
-          fillColor="red"
-        />
-
-        {/* 선긋기 */}
-        <Polyline
-          strokeWidth={2}
-          strokeColor='blue'
-          coordinates={[
-            { latitude: 36.7898, longitude: 127.0012 },
-            { latitude: 36.7898, longitude: 127.0512 }
-          ]}
-        />
-
-        {/* 선긋기 자유자재 */}
-        <Polygon
-          strokeWidth={2}
-          strokeColor='blue'
-          coordinates={[
-            { latitude: 36.7898, longitude: 127.0012 },
-            { latitude: 36.7898, longitude: 127.0012 },
-            { latitude: 36.7898, longitude: 127.0012 },
-            { latitude: 36.7898, longitude: 127.0012 }
-          ]}
-        />
-
-          {/* 경로 선꾸미기 */}
-        {origin != undefined && destination != undefined && (
+        )}
+        {destination && (
+          <Marker
+            draggable
+            coordinate={destination}
+            onDragEnd={(e) => console.log('목적지 드래그 좌표: ', e.nativeEvent.coordinate)}
+          />
+        )}
+        {movingMarkerPosition && (
+          <Marker coordinate={movingMarkerPosition}>
+            <MyCustomMarkerView />
+          </Marker>
+        )}
+        {origin && destination && (
           <MapViewDirections
             origin={origin}
-            strokeColor='blue'
-            strokeWidth={2}
             destination={destination}
             apikey={GOOGLE_MAPS_API_KEY}
+            strokeColor='blue'
+            strokeWidth={2}
+            onReady={(result) => {
+              console.log(result.coordinates);
+              setRouteCoordinates(result.coordinates);
+              animateMarker();
+            }}
           />
         )}
       </MapView>
-      
-   
     </SafeAreaView>
-  )
+  );
 }
 
-// 스타일 시트
-
 const styles = StyleSheet.create({
-  // safearea
   container: {
     flex: 1,
   },
-
-  //지도
   map: {
     position: 'absolute',
     top: 0,
@@ -225,8 +159,6 @@ const styles = StyleSheet.create({
     bottom: 0,
     zIndex: 0,
   },
-
-  //출발지, 목적지 검색
   searchContainer: {
     position: 'absolute',
     top: 50,
