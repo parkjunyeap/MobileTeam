20336c3da5a34f4932879b3bcff64b2ea84a50ec
Sungyh305 <127086688+Sungyh305@users.noreply.github.com>
2023-12-07 20:03:59 +0900

    현재 위치 바로 설정됨
    .

--- mobileProject/src/screens/TaxiTouch.js
+++ mobileProject/src/screens/TaxiTouch.js
@@ -1,18 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { View, Image, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { View, StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_API_KEY } from '../config/constants';
-import MapViewDirections from 'react-native-maps-directions';
 import * as Location from 'expo-location';
-import { mapStyle } from '../global/mapStyle';
 
 export default function GoogleMapsScreen() {
   const mapRef = useRef(null);
+  const autocompleteRef = useRef(null);
   const [origin, setOrigin] = useState(null);
   const [destination, setDestination] = useState(null);
-  const [routeCoordinates, setRouteCoordinates] = useState([]);
-  const [carMarkerPosition, setCarMarkerPosition] = useState(null);
 
   async function getCurrentLocation() {
     try {
@@ -26,119 +23,86 @@ export default function GoogleMapsScreen() {
       const { latitude, longitude } = location.coords;
       setOrigin({ latitude, longitude });
       moveToLocation(latitude, longitude);
+
+      const addressResponse = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude
+      });
+      const address = addressResponse[0];
+      const formattedAddress = `${address.street}, ${address.city}, ${address.region}, ${address.postalCode}, ${address.country}`;
+      
+      if (autocompleteRef.current) {
+        autocompleteRef.current.setAddressText(formattedAddress);
+      }
     } catch (error) {
       console.error('Error getting current location', error);
     }
   }
 
-  async function moveToLocation(latitude, longitude) {
+  function moveToLocation(latitude, longitude) {
     mapRef.current.animateToRegion({
       latitude,
       longitude,
-      latitudeDelta: 0.015,
-      longitudeDelta: 0.0121,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
     }, 2000);
   }
 
   useEffect(() => {
     getCurrentLocation();
-
-    if (origin && destination && routeCoordinates.length) {
-      animateMarker(routeCoordinates);
-    }
-  }, [origin, destination, routeCoordinates]);
-
-  const animateMarker = (coordinates) => {
-    let step = 0;
-    const numSteps = coordinates.length - 1;
-    const interval = setInterval(() => {
-      if (step <= numSteps) {
-        setCarMarkerPosition(coordinates[step]);
-        step++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 1000);
-  };
+  }, []);
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <View style={styles.searchContainer}>
-          {/* 출발지 검색 */}
-          <GooglePlacesAutocomplete
-            placeholder='출발지'
-            fetchDetails={true}
-            onPress={(data, details = null) => {
-              if (details && details.geometry && details.geometry.location) {
-                const { lat, lng } = details.geometry.location;
-                setOrigin({ latitude: lat, longitude: lng });
-                moveToLocation(lat, lng);
-              } else {
-                console.error('No location details available');
-              }
+        <GooglePlacesAutocomplete
+          ref={autocompleteRef}
+          placeholder='출발지'
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            const { lat, lng } = details.geometry.location;
+            setOrigin({ latitude: lat, longitude: lng });
+            moveToLocation(lat, lng);
+          }}
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: 'ko',
+          }}
+            styles={{
+              container: { flex: 0 },
+              textInput: { paddingLeft: 20, height: 40 },
             }}
-            query={{
-              key: GOOGLE_MAPS_API_KEY,
-              language: 'ko',
-            }}
-          />
-          {/* 목적지 검색 */}
-          <GooglePlacesAutocomplete
-            placeholder='목적지'
-            fetchDetails={true}
-            onPress={(data, details = null) => {
-              if (details && details.geometry && details.geometry.location) {
-                const { lat, lng } = details.geometry.location;
-                setDestination({ latitude: lat, longitude: lng });
-                moveToLocation(lat, lng);
-              } else {
-                console.error('No location details available');
-              }
-            }}
-            query={{
-              key: GOOGLE_MAPS_API_KEY,
-              language: 'ko',
-            }}
-          />
-        </View>
+        
+        />
+        <GooglePlacesAutocomplete
+          placeholder='목적지'
+          fetchDetails={true}
+          onPress={(data, details = null) => {
+            const { lat, lng } = details.geometry.location;
+            setDestination({ latitude: lat, longitude: lng });
+          }}
+          query={{
+            key: GOOGLE_MAPS_API_KEY,
+            language: 'ko',
+          }}
+          styles={{
+            container: { flex: 0 },
+            textInput: { paddingLeft: 20, height: 40 },
+          }}
+        />
         <MapView
           ref={mapRef}
           provider={PROVIDER_GOOGLE}
           style={styles.map}
           initialRegion={{
-            latitude: origin?.latitude || 0,
-            longitude: origin?.longitude || 0,
-            latitudeDelta: 0.1,
-            longitudeDelta: 0.1,
+            latitude: origin?.latitude ?? 0,
+            longitude: origin?.longitude ?? 0,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
           }}
-          customMapStyle={mapStyle}
         >
           {origin && <Marker coordinate={origin} />}
           {destination && <Marker coordinate={destination} />}
-          {carMarkerPosition && (
-            <Marker coordinate={carMarkerPosition}>
-              <Image
-                style={styles.carMarker}
-                source={require('../../assets/carMarker.png')}
-              />
-            </Marker>
-          )}
-          {origin && destination && (
-            <MapViewDirections
-              origin={origin}
-              destination={destination}
-              apikey={GOOGLE_MAPS_API_KEY}
-              strokeWidth={3}
-              strokeColor='hotpink'
-              onReady={(result) => {
-                setRouteCoordinates(result.coordinates);
-                if (result.coordinates.length) {
-                  animateMarker(result.coordinates);
-                }
-              }}
-            />
-          )}
         </MapView>
       </View>
     </SafeAreaView>
@@ -152,19 +116,27 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
   container: {
-    ...StyleSheet.absoluteFillObject,
+    flex: 1,
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    flex: 1,
   },
-  searchContainer: {
-    position: 'absolute',
-    top: Platform.OS === 'ios' ? 60 : 80,
+  searchBarContainer: {
     width: '100%',
-    zIndex: 10,
+    zIndex: 1,
   },
-  carMarker: {
-    width: 32,
-    height: 32,
+  textInputContainer: {
+    backgroundColor: 'rgba(255,255,255,0)',
+    borderTopWidth: 0,      // 상단 테두리 없앰
+    borderBottomWidth: 0,   // 하단 테두리 없앰
+    height: 38,   
+  },
+  textInput: {
+    marginLeft: 0,
+    marginRight: 0,
+    height: 38,             // `textInputContainer`와 동일하게 설정
+    fontSize: 16,           // 폰트 사이즈 조정
+    borderRadius: 5,        // 입력 필드의 둥근 모서리 설정
+    padding: 0, 
   },
 });
