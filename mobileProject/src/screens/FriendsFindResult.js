8fc1173462f2efee78508fe9c21dc391a5cb6a64
dayzononedaypack <whathumen@gmail.com>
2023-12-11 20:20:11 +0900

    ip 변경

--- mobileProject/src/screens/FriendsFindResult.js
+++ mobileProject/src/screens/FriendsFindResult.js
@@ -1,20 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import { StyleSheet, Text, View, ScrollView } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import User from "../components/User";
+import { UserType } from "../UserContext"; // 예시로 사용자 컨텍스트를 임포트
 
 const FriendFindResult = () => {
   const route = useRoute();
   const { userPCs, userSEs } = route.params || {};
+  const { userId: currentUserId } = useContext(UserType); // 현재 로그인한 사용자의 _id
 
-  // 중복 제거 로직: 사용자의 _id를 기반으로 중복 제거
+  // 중복 제거 및 현재 사용자 제외 로직
   const uniqueUserIds = new Set();
   const combinedUsers = [...userPCs, ...userSEs].filter(user => {
-    if (!uniqueUserIds.has(user._id)) {
-      uniqueUserIds.add(user._id);
-      return true;
-    }
-    return false;
+    return user._id !== currentUserId && !uniqueUserIds.has(user._id) && uniqueUserIds.add(user._id);
   });
 
   return (
