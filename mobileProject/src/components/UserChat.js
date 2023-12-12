5190073f3ad3b4beee8c06868819a86afadd0c1c
dayzononedaypack <whathumen@gmail.com>
2023-12-12 19:40:18 +0900

    다시 ip주소 localhost로 바꿨으니 쓸때 고치셈

--- mobileProject/src/components/UserChat.js
+++ mobileProject/src/components/UserChat.js
@@ -12,7 +12,7 @@ const UserChat = ({ item }) => {
   const fetchMessages = async () => {
     try {
       const response = await fetch(
-        `http://192.168.219.104:8000/messages/${userId}/${item._id}`
+        `http://localhost:8000/messages/${userId}/${item._id}`
       );
       const data = await response.json();
 
